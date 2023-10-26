5d84f2dba5d8f86056a187aa6329d93b424e6628
mohdjariullah <mohdjariullah@gmail.com>
2023-10-26 23:13:20 +0530

    Login

---
+++ client/src/pages/SucessPage.js
@@ -0,0 +1,12 @@
+function Sucess() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sucess</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Sucess;
+  
